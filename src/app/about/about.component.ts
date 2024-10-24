26b0b6cf8b9b20af356fb4a529357d2a8dc4eae7
Joseph James Smyth <jjsmyth1984@gmail.com>
2024-10-24 17:28:39 +0100

    Basic dependency injection using get to obtain an json object with data and process it on a page using a for loop.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,5 +1,6 @@
-import {Component, inject} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgClass, NgForOf} from "@angular/common";
+import {UserService} from "../../services/user.service";
 
 @Component({
     selector: 'app-about',
@@ -8,13 +9,26 @@ import {NgClass, NgForOf} from "@angular/common";
         NgClass,
         NgForOf
     ],
+    providers: [UserService],
     templateUrl: './about.component.html',
     styleUrls: ['./about.component.scss']
 })
 
-export class AboutComponent {
+export class AboutComponent implements OnInit {
 
-    constructor() {
+    users = [];
+
+    // Constructor inject UserService
+    constructor(private userService: UserService) {}
+
+    ngOnInit() {
+        this.getUsers();
+    }
+
+    getUsers() {
+        this.userService.getUsers().subscribe((users:any)=> {
+            this.users = users;
+        });
     }
 
 }
